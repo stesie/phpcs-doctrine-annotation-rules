0db2fce8cc083126800be603ddf83d6cf1ff20a6
Stefan Siegl <stesie@brokenpipe.de>
2017-08-04 23:12:37 +0200

    test AbstractDoctrineAnnotationSniff::getImports

--- tests/DoctrineAnnotationCodingStandard/Sniffs/Commenting/AbstractDoctrineAnnotationSniffTest.php
+++ tests/DoctrineAnnotationCodingStandard/Sniffs/Commenting/AbstractDoctrineAnnotationSniffTest.php
@@ -27,6 +27,19 @@ class AbstractDoctrineAnnotationSniffTest extends TestCase
         $this->assertSame('Foo\\Bar\\Baz', $this->getSniff()->getNamespace());
     }
 
+    public function testSimpleUse()
+    {
+        $this->checkFile(__DIR__ . '/data/SimpleUse.inc', DummySniff::class);
+        $this->assertEquals(['baz' => 'Foo\\Bar\\Baz'], $this->getSniff()->getImports());
+    }
+
+    public function testUseWithRename()
+    {
+        $this->checkFile(__DIR__ . '/data/UseWithRename.inc', DummySniff::class);
+        $this->assertEquals(['testling' => 'Foo\\Bar\\Baz'], $this->getSniff()->getImports());
+    }
+
+
     private function getSniff(): DummySniff
     {
         $sniff = reset($this->codeSniffer->ruleset->sniffs);
